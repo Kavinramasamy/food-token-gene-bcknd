2b334fa8ae2a0efde5111cee34ca09359680e065
Kavinramasamy <kavinramasamymech@gmail.com>
2024-01-06 16:41:32 +0530

    update

--- router/routers.js
+++ router/routers.js
@@ -1,29 +1,29 @@
 import express from "express";
 import WelcomeComp from "../component/welcome/welcome.js";
-import AdminLogin from "../component/admin/admin_login.js";
-import AdminSignUp from "../component/admin/admin_signup.js";
-import AddNewFood from "../component/food/add_new_food.js";
-import FoodList from "../component/food/food_list.js";
-import AddNewFoodOrder from "../component/order/order_food.js";
-import OrderList from "../component/order/order.js";
-import EditFoodOrder from "../component/order/edit_order.js";
+// import AdminLogin from "../component/admin/admin_login.js";
+// import AdminSignUp from "../component/admin/admin_signup.js";
+// import AddNewFood from "../component/food/add_new_food.js";
+// import FoodList from "../component/food/food_list.js";
+// import AddNewFoodOrder from "../component/order/order_food.js";
+// import OrderList from "../component/order/order.js";
+// import EditFoodOrder from "../component/order/edit_order.js";
 
 const router = express.Router();
 
 // WELCOME
 router.get("/", WelcomeComp);
 
-// ADMIN
-router.post("/login", AdminLogin);
-router.post("/signup", AdminSignUp);
+// // ADMIN
+// router.post("/login", AdminLogin);
+// router.post("/signup", AdminSignUp);
 
-// FOOD
-router.get("/food", FoodList);
-router.post("/food", AddNewFood);
+// // FOOD
+// router.get("/food", FoodList);
+// router.post("/food", AddNewFood);
 
-// ORDER
-router.get("/order", OrderList);
-router.post("/order", AddNewFoodOrder);
-router.put("/order", EditFoodOrder);
+// // ORDER
+// router.get("/order", OrderList);
+// router.post("/order", AddNewFoodOrder);
+// router.put("/order", EditFoodOrder);
 
 export default router;
