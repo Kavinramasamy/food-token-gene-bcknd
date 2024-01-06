d5ac50f812b5a031e6bfd165efb83ca7a1de6011
Kavinramasamy <kavinramasamymech@gmail.com>
2024-01-06 16:44:42 +0530

    update

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import "dotenv/config.js";
 import { db_connection } from "./database/db_connection.js";
 import router from "./router/routers.js";
+import WelcomeComp from "./component/welcome/welcome.js";
 
 const app = express();
 
@@ -12,7 +13,8 @@ app.use(express.json());
 db_connection();
 
 //Router
-app.use("/", router);
+app.get("/", WelcomeComp);
+// app.use("/", router);
 
 //Server
 const PORT = process.env.PORT;
