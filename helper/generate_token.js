5a66d3d530701fb6ecc091d4b7caab228324811a
Kavinramasamy <kavinramasamymech@gmail.com>
2024-02-22 00:05:44 +0530

    update

--- helper/generate_token.js
+++ helper/generate_token.js
@@ -4,7 +4,7 @@ import "dotenv/config.js";
 const generate_token = async (email) => {
   const key = process.env.KEY;
 
-  return jwt.sign({ email }, key, { expiresIn: "1d" });
+  return jwt.sign({ id: email }, key, { expiresIn: "1d" });
 };
 
 export default generate_token;
